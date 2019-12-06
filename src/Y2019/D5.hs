7106e156c5e255bc0fcceb166fb0461de3301a36
Lúcás Meier <cronokirby@gmail.com>
2019-12-06 11:05:55 +0100

    Finish d5

--- src/Y2019/D5.hs
+++ src/Y2019/D5.hs
@@ -26,19 +26,20 @@ class Monad m => CodeM m where
     output :: Int -> m ()
     input :: m Int
 
-data OPType = Mul | Add | Input | Output | Halt
+data OPType = Mul | Add | Input | Output | JT | JF | SLT | SE | Halt deriving (Eq, Show)
 
-data Mode = Imm | Pos
+data Mode = Imm | Pos deriving (Eq, Show)
 
-data OP = OP OPType Mode Mode Mode
+data OP = OP OPType Mode Mode Mode deriving (Eq, Show)
 
 decodeOP :: Int -> OP
 decodeOP i =
-    let (opcode, i1) = divMod i 100
-        (mode1, i2) = divMod i1 10
-        (mode2, i3) = divMod i2 10
-        (mode3, _) = divMod i3 10
-    in OP (decodeType opcode) (mode mode1) (mode mode2) (mode mode3)
+    let (i1, opcode) = divMod i 100
+        (i2, mode1) = divMod i1 10
+        (i3, mode2) = divMod i2 10
+        (_, mode3) = divMod i3 10
+        o = OP (decodeType opcode) (mode mode1) (mode mode2) (mode mode3)
+    in o
   where
     mode :: Int -> Mode
     mode 0 = Pos
@@ -48,11 +49,16 @@ decodeOP i =
     decodeType 2 = Mul
     decodeType 3 = Input
     decodeType 4 = Output
+    decodeType 5 = JT
+    decodeType 6 = JF
+    decodeType 7 = SLT
+    decodeType 8 = SE
     decodeType _ = Halt
 
 execute :: CodeM m => Int -> m ()
 execute pc = do
-    (OP typ m1 m2 m3) <- decodeOP <$> read pc
+    op <- read pc
+    let (OP typ m1 m2 m3) = decodeOP op
     case typ of
         Add -> do
             a1 <- getValue m1 (pc + 1)
@@ -75,46 +81,72 @@ execute pc = do
             a1 <- getValue m1 (pc + 1)
             output a1
             execute (pc + 2)
+        JT -> do
+            a1 <- getValue m1 (pc + 1)
+            a2 <- getValue m2 (pc + 2)
+            let pc' = if a1 /= 0 then a2 else pc + 3
+            execute pc'
+        JF -> do
+            a1 <- getValue m1 (pc + 1)
+            a2 <- getValue m2 (pc + 2)
+            let pc' = if a1 == 0 then a2 else pc + 3
+            execute pc'
+        SLT -> do
+            a1 <- getValue m1 (pc + 1)
+            a2 <- getValue m2 (pc + 2)
+            o <- read (pc + 3)
+            let w = if a1 < a2 then 1 else 0
+            write o w
+            execute (pc + 4)
+        SE -> do
+            a1 <- getValue m1 (pc + 1)
+            a2 <- getValue m2 (pc + 2)
+            o <- read (pc + 3)
+            let w = if a1 == a2 then 1 else 0
+            write o w
+            execute (pc + 4)
         Halt -> return ()
   where
     getValue :: CodeM m => Mode -> Index -> m Int
     getValue Imm ix = read ix
     getValue Pos ix = read ix >>= read
 
-type ConcreteCodeM s = ReaderT (MV.MVector s Int, STRef s [Int]) (ST.ST s)
+type ConcreteCodeM s = ReaderT (MV.MVector s Int, STRef s [Int], Int) (ST.ST s)
 
 instance CodeM (ConcreteCodeM s) where
     read ix = do
-        vec <- asks fst
+        (vec, _, _) <- ask
         lift $ MV.read vec ix
     write ix v = do
-        vec <- asks fst
+        (vec, _, _) <- ask
         lift $ MV.write vec ix v
     output v = do
-        outputs <- asks snd
+        (_, outputs, _) <- ask
         lift $ modifySTRef' outputs (v:)
-    input = return 1
+    input = asks (\(_, _, i) -> i)
 
 
-runConcreteCode :: Input -> [Int]
-runConcreteCode input = ST.runST $ do
+runConcreteCode :: Int -> Input -> [Int]
+runConcreteCode i input = ST.runST $ do
     vec <- V.unsafeThaw (V.fromList input)
     outputs <- newSTRef []
-    runReaderT (execute 0 *> readOutputs) (vec, outputs)
+    runReaderT (execute 0 *> readOutputs) (vec, outputs, i)
   where
     readOutputs :: ConcreteCodeM s [Int]
     readOutputs = do
-        outputs <- asks snd
+        (_, outputs, _) <- ask
         lift $ readSTRef outputs
 
 
 solve1 :: Input -> Int
-solve1 input = case runConcreteCode input of
+solve1 input = case runConcreteCode 1 input of
     (o:_) -> o
-    _ -> error "No outputs"
+    _     -> error "No outputs"
 
 solve2 :: Input -> Int
-solve2 = solve1
+solve2 input = case runConcreteCode 5 input of
+    (o:_) -> o
+    _     -> error "No outputs"
 
 theSolution :: A.Solution Input Int Int
 theSolution = A.Solution readInput show show solve1 solve2
