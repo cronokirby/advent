7d172c10056191d9752cdcc5bfb4996ad1a0f04a
Lúcás Meier <cronokirby@gmail.com>
2020-12-02 06:41:38 +0100

    Setup Ourlude, optionally

---
+++ src/Ourlude.hs
@@ -0,0 +1,36 @@
+module Ourlude
+  ( module Relude,
+    (|>),
+    (<|),
+    (>>>),
+    (<<<),
+  )
+where
+
+import Relude hiding ((<<<), (>>>))
+
+infixl 1 |>
+
+-- A forward composition operator, that I find prettier than (&)
+(|>) :: a -> (a -> b) -> b
+x |> f = f x
+{-# INLINE (|>) #-}
+
+infixr 0 <|
+
+-- A backwards composition operator, that I find prettier than ($)
+(<|) :: (a -> b) -> a -> b
+(<|) = ($)
+{-# INLINE (<|) #-}
+
+infixr 9 <<<
+
+(<<<) :: (b -> c) -> (a -> b) -> (a -> c)
+g <<< f = g . f
+{-# INLINE (<<<) #-}
+
+infixl 9 >>>
+
+(>>>) :: (a -> b) -> (b -> c) -> (a -> c)
+f >>> g = g . f
+{-# INLINE (>>>) #-}
