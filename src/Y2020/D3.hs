b07ab798699e455e680ef0dff41e3d472d8c5edc
Lúcás Meier <cronokirby@gmail.com>
2020-12-03 06:49:54 +0100

    Add day 3

--- src/Y2020/D3.hs
+++ src/Y2020/D3.hs
@@ -1,21 +1,55 @@
 module Y2020.D3 (problem) where
 
 import qualified Advent as A
+import qualified Data.Set as Set
 import Ourlude
 
-type Input = ()
+type Pos = (Int, Int)
 
-readInput :: Text -> Maybe Input
-readInput _ = Just ()
+addPos :: Pos -> Pos -> Pos
+addPos (x, y) (x', y') = (x + x', y + y')
 
-solve1 :: Input -> ()
-solve1 _ = ()
+data Grid = Grid
+  { occupied :: Set.Set Pos,
+    width :: Int,
+    height :: Int
+  }
+  deriving (Eq, Show)
 
-solve2 :: Input -> ()
-solve2 _ = ()
+isInBounds :: Grid -> Pos -> Bool
+isInBounds (Grid _ w h) (_, y) = y >= 0 && y < h
 
-theSolution :: A.Solution Input () ()
+isOccupied :: Grid -> Pos -> Bool
+isOccupied (Grid occupied' w _) (x, y) = Set.member (x `mod` w, y) occupied'
+
+wouldEncounter :: Grid -> Pos -> Int
+wouldEncounter grid step =
+  iterate (addPos step) (0, 0)
+    |> takeWhile (isInBounds grid)
+    |> filter (isOccupied grid)
+    |> length
+
+readInput :: Text -> Maybe Grid
+readInput txt = do
+  let theLines = lines txt |> map toString
+      theHeight = length theLines
+  theWidth <- viaNonEmpty (head >>> length) theLines
+  let positioned =
+        zip [0 ..] theLines >>= \(y, line) -> zip [0 ..] line |> map (\(x, c) -> ((x, y), c))
+      occupied' = positioned |> filter (snd >>> (== '#')) |> map fst |> Set.fromList
+  Just (Grid occupied' theWidth theHeight)
+
+solve1 :: Grid -> Int
+solve1 grid = wouldEncounter grid (3, 1)
+
+solve2 :: Grid -> Int
+solve2 grid =
+  [(3, 1), (1, 1), (5, 1), (7, 1), (1, 2)]
+    |> map (wouldEncounter grid)
+    |> product
+
+theSolution :: A.Solution Grid Int Int
 theSolution = A.Solution readInput show show solve1 solve2
 
 problem :: A.Problem
-problem = A.Problem theSolution "data/prompt-2020-2.txt" [] [] [] [] (A.ProblemInfo "TODO" 2020 3)
+problem = A.Problem theSolution "data/prompt-2020-3.txt" [] [] [] [] (A.ProblemInfo "TODO" 2020 3)
